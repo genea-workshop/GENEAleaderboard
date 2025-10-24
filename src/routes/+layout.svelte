daeb476db53a39279437c1c2655246415f7552c0
Mihail <tsakovm@gmail.com>
2025-10-24 11:39:47 +0200

    add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,6 +21,18 @@
 
 <svelte:head>
   <link rel="icon" href={favicon} />
+
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y8YVY5DTXK"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-Y8YVY5DTXK');
+  </script>
+
 </svelte:head>
 
 <div class="navbar-wrap">
