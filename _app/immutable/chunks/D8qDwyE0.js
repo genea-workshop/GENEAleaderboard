f5e7b2e99fe40be3131d8ae927959813e456fa0c
MikeTsakov <MikeTsakov@users.noreply.github.com>
2025-10-24 09:15:07 +0000

    deploy: 0195ef2243323de44cab7118b34333d40c2576fe

---
+++ _app/immutable/chunks/D8qDwyE0.js
@@ -0,0 +1,1 @@
+import{M as a}from"./_77SEeBZ.js";a();
---
+++ _app/immutable/nodes/8.GD4yIkTM.js
@@ -0,0 +1,16 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/D8qDwyE0.js";import{p,f as m,t as v,a as u,b as g,d as e,s as i,r as a,n as l}from"../chunks/_77SEeBZ.js";import{r as f,s as b}from"../chunks/DWryhkUO.js";import{i as k}from"../chunks/D5vw3C0O.js";var x=m(`<main class="subcontainer"><div class="home-split-ver"><div class="home-split-ver card"><h1 class="h1">Visualization Tools</h1> <div class="home-text card"><h1 class="h1">BVHView</h1> <p class="lead">Welcome to the <strong>GENEA Leaderboard</strong> project!  
+          This platform provides a unified evaluation hub for gesture generation and animation research.
+          Here you can explore state-of-the-art models, compare team results, and discover available datasets
+          to help you get started.</p> <a target="_blank" class="links">BVH View</a></div> <div class="home-text card"><h1 class="h1">Blender</h1> <p class="lead">Welcome to the <strong>GENEA Leaderboard</strong> project!  
+          This platform provides a unified evaluation hub for gesture generation and animation research.
+          Here you can explore state-of-the-art models, compare team results, and discover available datasets
+          to help you get started.</p> <a href="https://github.com/TeoNikolov/genea_visualizer/tree/dev-2025" target="_blank" class="links">Blender</a></div></div> <div class="home-split-ver card"><h1 class="h1">Datasets</h1> <div class="home-text card"><h1 class="h1">Talking With Hands</h1> <p class="lead">Welcome to the <strong>GENEA Leaderboard</strong> project!  
+          This platform provides a unified evaluation hub for gesture generation and animation research.
+          Here you can explore state-of-the-art models, compare team results, and discover available datasets
+          to help you get started.</p> <a href="https://github.com/facebookresearch/TalkingWithHands32M" target="_blank" class="links">Talking with Hands</a></div> <div class="home-text card"><h1 class="h1">SMPL-X</h1> <p class="lead">Welcome to the <strong>GENEA Leaderboard</strong> project!  
+          This platform provides a unified evaluation hub for gesture generation and animation research.
+          Here you can explore state-of-the-art models, compare team results, and discover available datasets
+          to help you get started.</p> <a href="https://smpl-x.is.tue.mpg.de/index.html" target="_blank" class="links">SMPL-X</a></div> <div class="home-text card"><h1 class="h1">Nymeria</h1> <p class="lead">Welcome to the <strong>GENEA Leaderboard</strong> project!  
+          This platform provides a unified evaluation hub for gesture generation and animation research.
+          Here you can explore state-of-the-art models, compare team results, and discover available datasets
+          to help you get started.</p> <a href="https://www.projectaria.com/datasets/nymeria/" target="_blank" class="links">Nymeria</a></div></div></div></main>`);function w(d,n){p(n,!1),k();var t=x(),s=e(t),r=e(s),o=i(e(r),2),h=i(e(o),4);a(o),l(2),a(r),l(2),a(s),a(t),v(c=>b(h,"href",c),[()=>f("/BVHView/bvhview.html")]),u(d,t),g()}export{w as component};
