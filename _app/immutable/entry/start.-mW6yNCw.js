a940c397ce92fc2b7adc662b26e4978b7416159a
MikeTsakov <MikeTsakov@users.noreply.github.com>
2025-10-26 15:21:07 +0000

    deploy: f36d812ca8f3da801f6d307f73a24453a186ba6a

--- _app/immutable/entry/start.-mW6yNCw.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CsLp9a2u.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.hd64o22Y.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BLJ8VnFW.js";export{o as load_css,r as start};
